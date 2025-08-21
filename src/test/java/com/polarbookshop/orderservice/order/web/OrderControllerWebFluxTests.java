039e53d13a3127b2a6fa22577cf821c083835d73
cesar-alejandro-perez <cesar.alejandro.perez@gmail.com>
2025-08-20 19:45:43 -0600

    auth

--- src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
+++ src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
@@ -1,11 +1,15 @@
 package com.polarbookshop.orderservice.order.web;
 
+import com.polarbookshop.orderservice.config.SecurityConfig;
 import com.polarbookshop.orderservice.order.domain.Order;
 import com.polarbookshop.orderservice.order.domain.OrderService;
 import com.polarbookshop.orderservice.order.domain.OrderStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
@@ -14,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @WebFluxTest(OrderController.class)
+@Import(SecurityConfig.class)
 class OrderControllerWebFluxTests {
 
 	@Autowired
@@ -30,6 +35,8 @@ class OrderControllerWebFluxTests {
 				.willReturn(Mono.just(expectedOrder));
 
 		webClient
+				.mutateWith(SecurityMockServerConfigurers.mockJwt()
+						.authorities(new SimpleGrantedAuthority("ROLE_customer")))
 				.post()
 				.uri("/orders")
 				.bodyValue(orderRequest)
